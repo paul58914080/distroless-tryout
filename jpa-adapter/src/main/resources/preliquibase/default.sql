82368f37b7dd4665cfe6f51ed04e30189a82459b
Paul Williams <paul58914080@gmail.com>
2024-08-26 13:24:17 +0530

    refactor(jpa): rename user to users
    user is a keyword and cannot be used

--- jpa-adapter/src/main/resources/preliquibase/default.sql
+++ jpa-adapter/src/main/resources/preliquibase/default.sql
@@ -1,3 +1,3 @@
 CREATE SCHEMA IF NOT EXISTS ${spring.liquibase.liquibase-schema:LIQUIBASE};
-CREATE SCHEMA IF NOT EXISTS ${spring.liquibase.default-schema:USER};
+CREATE SCHEMA IF NOT EXISTS ${spring.liquibase.default-schema:USERS};
 CREATE SCHEMA IF NOT EXISTS ${spring.jpa.properties.org.hibernate.envers:USER_AUDIT};
